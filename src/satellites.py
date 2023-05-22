b58c7425564dd52ad4c3d59d1bb75c0622f19b81
Martin D'Agostino <martin.dagostino@klarna.com>
2023-05-22 18:34:34 +0200

    satellite trayectory

--- src/satellites.py
+++ src/satellites.py
@@ -3,20 +3,61 @@ from skyfield.api import Topos, load
 # Load TLE data (replace with specific TLE lines)
 filepath = "../data/last_30_days_launches.tle"
 satellites = load.tle_file(filepath)
-satellite = satellites[0]
+satellite = satellites[15]
 
 # Get your desired time (UTC)
 ts = load.timescale()
-t = ts.utc(2023, 5, 20, 0, 0, 0)  # Replace with preferred date and time
+# t = ts.utc(2023, 5, 22, 15, 20, 0)  # Replace with preferred date and time
+#
+# # Calculate satellite position in ECEF coordinates
+# geocentric = satellite.at(t)
+# x, y, z = geocentric.position.km
+#
+# # Convert ECEF to latitude, longitude, and altitude
+# subpoint = geocentric.subpoint()
+# latitude = subpoint.latitude.degrees
+# longitude = subpoint.longitude.degrees
+# altitude = subpoint.elevation.km
+#
+# print(f"Latitude {latitude}\nLongitude {longitude}\nAltitude {altitude}")
 
-# Calculate satellite position in ECEF coordinates
-geocentric = satellite.at(t)
-x, y, z = geocentric.position.km
+import csv
+from datetime import datetime
+from datetime import timedelta
 
-# Convert ECEF to latitude, longitude, and altitude
-subpoint = geocentric.subpoint()
-latitude = subpoint.latitude.degrees
-longitude = subpoint.longitude.degrees
-altitude = subpoint.elevation.km
+now = datetime.now()
 
-print(f"Latitude {latitude}\nLongitude {longitude}\nAltitude {altitude}")
+
+with open('../data/last_30_days_launches.csv', 'w') as f:
+    writer = csv.writer(f)
+
+    writer.writerow(["Satellite", "Latitude", "Longitude"])
+    for i in range(30):
+        date = now - timedelta(days=i)
+        # convert datetime to Time
+        t = ts.utc(date.year, date.month, date.day, date.hour, date.minute, date.second)
+        geocentric = satellite.at(t)
+        x, y, z = geocentric.position.km
+        subpoint = geocentric.subpoint()
+        latitude = subpoint.latitude.degrees
+        longitude = subpoint.longitude.degrees
+        altitude = subpoint.elevation.km
+
+        print(f"Latitude {latitude}\nLongitude {longitude}\nAltitude {altitude}")
+        writer.writerow([satellite.name, latitude, longitude])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# writer.writerow(row)
